81ee9c164e9e84279c2d10ad9e37110806be5334
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-30 19:45:06 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function mapToNegativize(source) {
+ return source.map(num => num * -1)
+}
+
+function mapToNoChange(source) {
+  return source.map(num => num)
+}
+
+function mapToDouble(source) {
+  return source.map(num => num*2)
+}
+
+function mapToSquare(source) {
+  return source.map(num => num**2)
+}
+
+function reduceToTotal(source, starting = 0) {
+  return source.reduce(function(total, element){ return total + element}, starting)
+}
+
+function reduceToAllTrue(source) {
+  for (let i = 0; i < source.length; i++) {
+    if (!source[i]) {
+      return false
+    }
+  }
+  return true
+}
+
+function reduceToAnyTrue(source) {
+ for (let i = 0; i < source.length; i++) {
+    if (source[i]) {
+      return true
+    }
+  }
+  return false
+}
