956c8d533650914b38e4d323f514ae95cbf9ce33
Winston  Chung <18.chung.winston@ctkprep.org>
2020-04-30 18:49:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function mapToNegativize(source) {
 function mapToNoChange(source) {
   return source.map(num => num)
 }
+
+function mapToDouble(source) {
+  return source.map(num => num*2)
+}
+
+function mapToSquare(source) {
+  return source.map(num => num**2)
+}
